3d7d10d8beb74994188be3f4f04f86a2a284d444
Ayush <ayushvish6555@gmail.com>
2026-02-13 17:14:47 +0530

    fix

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void print_cli(){
+inline void print_cli(){
     cout << R"(
     ██████╗ ██████╗ ███████╗██╗   ██╗███╗   ██╗ ██████╗
     ██╔══██╗██╔══██╗██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝
